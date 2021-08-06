f6d6cde21faaa8f190c84f3075bd3abc9cbec7e3
dennisMSF <87782818+dennisMSF@users.noreply.github.com>
2021-08-05 22:20:54 -0500

    Create comissionemployee_2.h

---
+++ comissionemployee_2.h
@@ -0,0 +1,12 @@
+#include<string>
+#include"employee12.h"
+class BasePlusComissionEmployee:public ComissionEmployee{
+public:
+    BasePlusComissionEmployee(const string&,const string&,const string&,double=0.0,double=0.0,double=0.0);
+    void setBaseSalary(double);
+    double getBaseSalary()const;
+    double earnings()const;
+    void print()const;
+private:
+    double baseSalary;
+};
